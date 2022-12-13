42b1e304be2b14d3e9e0408d420691dcdc8567ac
ThurFerreira <arthurfsilva110@gmail.com>
2022-12-13 15:05:54 -0300

    ContaComum.java

--- LAB05 - Abstract classes/ex03/src/ContaComum.java
+++ LAB05 - Abstract classes/ex03/src/ContaComum.java
@@ -1,7 +1,5 @@
 public class ContaComum extends ContaCorrente{
-    private float limite;
     public ContaComum(float val, int num, int pwd) {
         super(val, num, pwd);
-        this.limite = 0;
     }
 }
